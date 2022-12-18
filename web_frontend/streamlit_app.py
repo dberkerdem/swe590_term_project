3f46f429d10548cfb45b4beed28d5234f1e42f65
dberkerdem <dberkerdem@gmail.com>
2022-12-19 01:33:57 +0300

    web_frontend is ready to deploy

--- web_frontend/streamlit_app.py
+++ web_frontend/streamlit_app.py
@@ -1,33 +1,40 @@
 import streamlit as st
-from utils.aws_tools import temporary_aws_handler
+from utils.aws_tools import write_image_to_s3
+# from streamlit_toggle import st_toggle_switch
+
+BUCKET_NAME = "swe590-bucket"
+DIR_NAME = "inputs/"
 
 
 def main():
     st.set_page_config(
-        page_title="Multipage App"
+        page_title="SWE-590",
+        layout="wide"
     )
 
-    st.title("Image Upload Page")
-    st.sidebar.success("Select a page above")
+    st.title("Upload your Images")
+    st.sidebar.success("Select a page")
 
     image_file = st.file_uploader("Upload An Image", type=['png', 'jpeg', 'jpg'], accept_multiple_files=True)
     for file in image_file:
         file.seek(0)
 
-    if image_file is not None:
-        for file in image_file:
-            # display the name and the type of the file
-            file_details = {"filename": file.name,
-                            "filetype": file.type
-                            }
-            st.write(file_details)
+    submit = st.button("Submit Images")
+    show_details = st.button("Show Upload Details")
 
-    submit = st.button("Submit")
+    if image_file is not None:
+        if show_details:
+            for file in image_file:
+                # display the name and the type of the file
+                file_details = {"filename": file.name,
+                                "filetype": file.type
+                                }
+                st.write(file_details)
 
     if image_file is not None:
         if submit:
             for file in image_file:
-                temporary_aws_handler(file=file)
+                write_image_to_s3(fileobj=file, bucket=BUCKET_NAME, key=DIR_NAME+file.name)
                 print('upload Successful')
                 st.success("Saved successfully!")
 
--- web_frontend/utils/aws_tools.py
+++ web_frontend/utils/aws_tools.py
@@ -1,17 +1,9 @@
 """aws_tools script contains utility functions to perform read and write operations to AWS"""
 import boto3
 
-# Specify the bucket name
-BUCKET_NAME = "swe590-bucket"
-AWS_REGION = "eu-west-1"
 
-
-def temporary_aws_handler(file):
-    # client = boto3.client("s3", region_name=AWS_REGION)
-
-    s3 = boto3.resource(
-        service_name='s3',
-        region_name=AWS_REGION,
-    )
-
-    s3.Bucket(BUCKET_NAME).put_object(Key='inputs/' + file.name, Body=file.type)
+def write_image_to_s3(fileobj: object, bucket: str, key: str) -> None:
+    """
+    """
+    conn = boto3.client("s3")
+    conn.upload_fileobj(Fileobj=fileobj, Bucket=bucket, Key=key)
