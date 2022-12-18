fc2d4ce582a713e89a0be581de2f12ebda745445
dberkerdem <dberkerdem@gmail.com>
2022-12-19 00:18:05 +0300

    Key exposure is fixed.

--- README.md
+++
@@ -1,2 +0,0 @@
-# -swe590_term_project
-This repository contains the term project of Cloud Computing class in Bogazici University SWE M.Sc. program.
--- pages/result.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import requests
-
-
-st.title("Result Page")
-
-results=requests.get('http://localhost:8888/tree/Untitled%20Folder%202')
-
-st.write(results)
