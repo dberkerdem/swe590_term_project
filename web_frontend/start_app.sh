b0cbae7909bf07fb6868860c75382b1152e6f9f1
dberkerdem <dberkerdem@gmail.com>
2022-12-19 10:39:08 +0300

    .gitignore tracking is fixed

---
+++ web_frontend/start_app.sh
@@ -0,0 +1,1 @@
+docker build -t web_frontend . && docker run -it -p 8501:8501 web_frontend
--- web_frontend/streamlit_app.py
+++ web_frontend/streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from utils.aws_tools import write_image_to_s3
 # from streamlit_toggle import st_toggle_switch
+import time
 
 BUCKET_NAME = "swe590-bucket"
 DIR_NAME = "inputs/"
@@ -12,15 +13,15 @@ def main():
         layout="wide"
     )
 
-    st.title("Upload your Images")
+    st.title("Upload.py your Images")
     st.sidebar.success("Select a page")
 
-    image_file = st.file_uploader("Upload An Image", type=['png', 'jpeg', 'jpg'], accept_multiple_files=True)
+    image_file = st.file_uploader("Upload.py An Image", type=['png', 'jpeg', 'jpg'], accept_multiple_files=True)
     for file in image_file:
         file.seek(0)
 
     submit = st.button("Submit Images")
-    show_details = st.button("Show Upload Details")
+    show_details = st.button("Show Upload.py Details")
 
     if image_file is not None:
         if show_details:
@@ -34,9 +35,12 @@ def main():
     if image_file is not None:
         if submit:
             for file in image_file:
-                write_image_to_s3(fileobj=file, bucket=BUCKET_NAME, key=DIR_NAME+file.name)
+                write_image_to_s3(fileobj=file, bucket=BUCKET_NAME, key=DIR_NAME + file.name)
                 print('upload Successful')
                 st.success("Saved successfully!")
+        # Wait 10 seconds after files are uploaded to s3 bucket
+        time.sleep(10)
+
 
 
 if __name__ == '__main__':
